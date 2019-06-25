0d5b0f72714627131a22408d595e09183a287bfe
lendkutia <52205050+lendkutia@users.noreply.github.com>
2019-06-25 15:14:19 +0200

    Added header styles and fixed header scripts

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 jQuery(document).ready(function($) {
- 
+ var navToggle = $(".js-nav-toggle"),
+    mainNav = $(".js-main-nav"),
+    win = $(window),
+    doc = $(document),
+    fixedHeader = $(".main_header--fixed");
+
+  /* NAV TOGGLE */
+  navToggle.click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass("is--active");
+    $(this)
+      .parent()
+      .find(mainNav)
+      .toggleClass("is--active");
+    $(this)
+      .parent()
+      .find(mainNav)
+      .fadeToggle(300);
+  });
+
+  /* HEADER FIXED */
+  win.scroll(function() {
+    if (doc.scrollTop() > 100) {
+      fixedHeader.addClass("is--active");
+    } else {
+      fixedHeader.removeClass("is--active");
+    }
+  });
 });
