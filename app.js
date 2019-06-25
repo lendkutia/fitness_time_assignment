6d242fa63c1221e1803840726656e77df322e16c
lendkutia <52205050+lendkutia@users.noreply.github.com>
2019-06-25 15:48:53 +0200

    Added Slider in Banner and Parallax in banner content

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($) {
- var navToggle = $(".js-nav-toggle"),
+  var navToggle = $(".js-nav-toggle"),
     mainNav = $(".js-main-nav"),
     win = $(window),
     doc = $(document),
-    fixedHeader = $(".main_header--fixed");
+    fixedHeader = $(".main_header--fixed"),
+    bannerContent = $(".hero-banner-item__content");
+    
 
   /* NAV TOGGLE */
   navToggle.click(function(e) {
@@ -26,5 +28,66 @@ jQuery(document).ready(function($) {
     } else {
       fixedHeader.removeClass("is--active");
     }
+    //OPacity Parallax on hero-content
+    var scrollTop = $(this).scrollTop();
+    bannerContent.css({
+      opacity: function() {
+        var elementHeight = $(this).height();
+        return 1 + ((elementHeight - scrollTop) / elementHeight) * 2;
+      }
+    });
+  });
+  /* SLIDER */
+  var slider = function(sliderElement) {
+    if (sliderElement.length > 1) {
+      sliderElement.each(function(index, element) {
+        slider(sliderElement.eq(index));
+      });
+      return;
+    }
+
+    var self = this,
+      slideIndex = 0,
+      innerSlider = sliderElement.find(" .slider-wrap"),
+      slides = innerSlider.find(" .hero-banner-item"),
+      firstSlide = slides.first(),
+      prevBtn = sliderElement.find(".prev-slide"),
+      nextBtn = sliderElement.find(".next-slide");
+
+    self.slideToIndex = function(index) {
+      var maxSlideIndex = innerSlider.find(" .hero-banner-item").length - 1;
+
+      if (index < 0) index = maxSlideIndex;
+      else if (index > maxSlideIndex) index = 0;
+
+      slideIndex = index;
+
+      var scrollStepSize = firstSlide.width();
+      var newScrollLeft = slideIndex * scrollStepSize;
+
+      innerSlider.stop().animate({ scrollLeft: newScrollLeft });
+    };
+
+    self.sliderItem = function(next) {
+      slideIndex = slideIndex + (next ? 1 : -1);
+      self.slideToIndex(slideIndex);
+    };
+
+    prevBtn.click(function(e) {
+      e.preventDefault();
+      self.sliderItem(false);
+    });
+    nextBtn.click(function(e) {
+      e.preventDefault();
+      self.sliderItem(true);
+    });
+
+    innerSlider.scroll();
+  };
+
+  $(function() {
+    var s = new slider($(".js-slider"));
+    s.sliderItem(true);
+    s.slideToIndex(3);
   });
 });
